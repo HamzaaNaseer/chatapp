07be824d4e7b5800160f5c131698690bf851677b
hamza naseer <hamzanaseer711@gmail.com>
2023-02-20 20:32:32 +0500

    auth done

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,29 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require("c:/users/invincible/desktop/chatpiyushyoutube/mern-chat-app/backend/middleware/errormiddleware");
+// const chatRoutes = require("./routes/chatRoutes");
+// const messageRoutes = require("./routes/messageRoutes");
+
 dotenv.config();
+connectDB();
+
+app.use(express.json()); // to accept json data
+
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
+
+app.use("/api/user", userRoutes);
+// app.use("/api/chat", chatRoutes);
+// app.use("/api/message", messageRoutes);
+
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
+
+
 const port = process.env.PORT || 5000;
 app.listen(5000, console.log(`server started on port ${port}`));
